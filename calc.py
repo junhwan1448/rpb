60fd56fc7b32d584b78796fae5b7388713607434
junhwan1448 <junhwan1448@gmail.com>
2026-04-03 02:21:46 +0900

    update calc.py

--- calc.py
+++ calc.py
@@ -1,1 +1,17 @@
 def main():
+    x = int(input("x > "))
+    y = int(input("y > "))
+    print("%d + %d = %d" % (x, y, add(x, y)))
+    print("%d / %d = %0.3f" % (x, y, divide(x, y)))
+   
+def add(a, b):
+    return a + b
+    
+def divide(a, b):
+    if b == 0:
+        print("Error: cannot divide by zero.")
+        return None
+    return round(a / b, 3)
+        
+if __name__ == "__main__":
+    main()
